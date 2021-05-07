ba9ce8b7dca20efa7938e574e2542275de79bff3
Kian Kasad <kian@kasad.com>
2021-05-07 00:27:23 -0700

    cd to home directory if no path given

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #include "builtins.h"
+#include "../paths.h"
 
 /*
  * Change directory builtin command
  */
 int kshell_builtin_cd(char **args)
 {
+	char *dest;
+
 	if (!args[1]) {
-		fprintf(stderr, PROGNAME": cd: expected argument\n");
-	} else if (chdir(args[1]) < 0) {
-		perror(PROGNAME": cd");
+		/* if no directory given, go to home directory */
+		dest = get_home_dir();
+		if (!dest) {
+			fputs(PROGNAME": unable to get home directory\n", stderr);
+			return 0;
+		}
+	} else {
+		/* try to go to given directory */
+		dest = args[1];
 	}
+
+	if (chdir(dest) < 0) {
+		perror(PROGNAME": cd");
+		goto done;
+	}
+
+done:
+	if (!args[1])
+		free(dest);
 
 	return 0;
 }
